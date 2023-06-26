6d5320c1bf69bfe113022c86c15a84f60def3b44
ZakiYoga <l200200229@student.ums.ac.id>
2023-06-26 19:58:21 +0700

    first commit

---
+++ .php_tmp/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Modul Praktikum Pemrograman Website</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo-navbar">PrakWeb</div>
+        <navbar class="navbar">
+            <ul>
+                <li>
+                    <a href="/">Beranda</a>
+                </li>
+                <li><a href="/">Practicum Module</a></li>
+                <li><a href="/">Quiz</a></li>
+            </ul>
+        </navbar>
+        <div class="btn-container">
+            <a class="signup" href="./component/page/login.php">Sign Up</a>
+            <a class="login" href="./component/page/register.php">Login</a>
+        </div>
+    </header>
+    <main>
+        <div class="container-main">
+            <div class="hero-content">
+                <h2>Praktikum Pemrograman Web</h2>
+                <p>Belajar Mudah, Kapanpun dan dimanapun
+                </p>
+                <button>Lihat Modul</button>
+            </div>
+            <div class="hero-img">
+            </div>
+        </div>
+        <div class="main-content">
+            <table>
+                <tr>
+                    <th>
+
+                    </th>
+                </tr>
+            </table>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
